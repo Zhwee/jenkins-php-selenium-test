b34f2aa92d6c8c2b183820aac2769b980752aa9a
FICE <77541619+Zhwee@users.noreply.github.com>
2024-07-23 15:12:11 +0800

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -8,6 +8,10 @@ git clone https://github.com/Zhwee/jenkins-php-selenium-test.git
 cd jenkins-php-selenium-test
 echo $(pwd)
 ls
+
+# Pull the PHP Docker image
+docker pull php:7.2-apache
+
 # Run the Docker container
 docker run -d -p 80:80 --name my-apache-php-app -v $(pwd)/src:/var/www/html php:7.2-apache
 sleep 1
