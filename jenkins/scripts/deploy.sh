c633bf515e4acd88a87cc5c5e6cb7dd040c66835
Zhwee <eternallingfice@gmail.com>
2024-07-23 13:46:06 +0800

    edited

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,19 +1,15 @@
-# #!/usr/bin/env sh
-
-# set -x
-# docker run -d -p 80:80 --name my-apache-php-app -v E:\\ict2216\\w8sel\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
-# sleep 1
-# set +x
-
-# echo 'Now...'
-# echo 'Visit http://192.168.10.250 to see your PHP application in action.'
-
 #!/usr/bin/env sh
-
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v /e/ict2216/w8sel/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
+# Clone the repository
+git clone https://github.com/Zhwee/jenkins-php-selenium-test.git
+
+# Change directory to the cloned repository
+cd jenkins-php-selenium-test
+
+# Run the Docker container
+docker run -d -p 80:80 --name my-apache-php-app -v $(pwd)/src:/var/www/html php:7.2-apache
 sleep 1
 set +x
 
 echo 'Now...'
-echo 'Visit http://192.168.10.250 to see your PHP application in action.'
+echo 'Visit http://localhost to see your PHP application in action.'
